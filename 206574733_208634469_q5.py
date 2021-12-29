611520a6a6dbf8e7be93e4ba702cec8b4bdb928e
Matan Shiloni <mshiloni22@gmail.com>
2021-12-29 20:51:46 +0200

    until KNN

--- 206574733_208634469_q5.py
+++ 206574733_208634469_q5.py
@@ -8,10 +8,14 @@ def unpickle(file):
         dict = pickle.load(fo, encoding='bytes')
     return dict
 
+# if it works, we need to add all other batch files to the list
 files_list = [r"C:\Users\mshil\PycharmProjects\Semester_03\AlgebricMethods_Course\206574733_208634469\cifar-10-batches-py\data_batch_1"]
 s_list = [5]
 k_list = [5]
 train_list = []
+reduced_data_dict = {} # will hold the reduced data for knn for each s value
+
+# step1: turning all data to grayscale, arranging data in one matrix
 for file in files_list:
     my_dict = unpickle(file)
     img = my_dict[b'data']
@@ -27,12 +31,18 @@ for file in files_list:
         train_list.append(my_vector)
 train_t = np.concatenate(train_list, axis=0)
 train = np.transpose(train_t)
+
+# step2: PCA and decreasing data's dimension, running knn per each s in s_list
 for s in s_list:
-    MIC_t = np.zeros((s, 1024))
+    Us_t = np.zeros((s, 1024))
     train_copy = np.matrix(train)
     for i in range(s):
         U, _, _ = np.linalg.svd(train_copy, full_matrices=False, compute_uv=True)
         next_u = np.transpose(U)[0]
-        MIC_t[i] = next_u
+        Us_t[i] = next_u
         train_copy = np.matmul((np.identity(1024) - (np.matmul(np.transpose(next_u), next_u))), train_copy)
-    MIC = np.transpose(MIC_t)
+    train_reduced = np.zeros((s, len(train[1].astype(int))))
+    for i in range(len(train[1])):
+        train_reduced[:, i] = np.matmul(Us_t, train[:, i])
+    reduced_data_dict[s] = train_reduced
+
